caa85efae48889f08a1ba1491a72d5280df3acca
Lope Labiyi <253978114+uptolope@users.noreply.github.com>
2026-05-07 11:31:30 -0500

    Remove build artifacts and add ignore rules

--- .next/static/chunks/pages/_app-2df37a9a9643f217.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{29763:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(24767)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[6593,8792],()=>(n(29763),n(53639))),_N_E=_.O()}]);
